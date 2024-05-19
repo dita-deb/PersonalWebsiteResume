8eb4c2ecd37e3aee070a42992ed2688c97e47ccd
Anindita Deb <aninditadeb.010@gmail.com>
2024-05-19 15:35:07 -0400

    Initial commit

---
+++ my-portfolio/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'my-portfolio';
+
+  displayResume() {
+    // Open the resume image in a new tab
+    window.open('assets/Anindita_Deb_Resume-1.png', '_blank');
+  }
+}
