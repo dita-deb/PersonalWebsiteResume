4f3b9e370b2716d97af92b7c28437129d2eb412c
Anindita Deb <aninditadeb.010@gmail.com>
2024-10-11 16:14:52 -0400

    changed resume and added certificates earned

--- my-portfolio/src/app/app.component.ts
+++ my-portfolio/src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent {
 
   displayResume() {
     // Open the resume image in a new tab
-    window.open('assets/Anindita_Deb_Resume.png', '_blank');
+    window.open('assets/Anindita_Deb_Resume.pdf', '_blank');
   }
 
 }
